2b734f9156545380775af09a6d99294eab8cf62f
Chichebe <chichebewebdev@gmail.com>
2023-05-25 10:06:30 -0700

    commit

--- millionaire-client/src/components/questionsAndAnwersBank.jsx
+++ millionaire-client/src/components/questionsAndAnwersBank.jsx
@@ -1,92 +1,232 @@
- const questionsAndAnswers = [
-    {
-        id:1,
-        question: "In the UK, the abbreviation NHS stands for National what Service?",
-        answers:[
-            {
-                text:"Health",
-                correct: true,
-            },
-            {
-                text:"Honour",
-                correct: false,
-            },
-            {
-                text:"Household",
-                correct: false,
-            },
-            {
-                text:"Humanity",
-                correct: false,
-            }
-        ]
-    },
-    {
-        id:2,
-        question: "Which of these brands was chiefly associated with the manufacture of household locks?",
-        answers:[
-            {
-                text:"Phillips",
-                correct: false,
-            },
-            {
-                text:"Flymo",
-                correct: false,
-            },
-            {
-                text:"Ronseal",
-                correct: false,
-            },
-            {
-                text:"Chubb",
-                correct: true,
-            }
-        ]
-    },
-    {
-        id:3,
-        question: "What name is given to the revolving belt machinery in an airport that delivers checked luggage from the plane to baggage reclaim?",
-        answers:[
-            {
-                text:"Carousel",
-                correct: true,
-            },
-            {
-                text:"Hangar",
-                correct: false,
-            },
-            {
-                text:"Concourse",
-                correct: false,
-            },
-            {
-                text:"Terminal",
-                correct: false,
-            }
-        ]
-    },
-    {
-        id:4,
-        question: "Which Disney character famously leaves a glass slipper behind at a royal ball?",
-        answers:[
-            {
-                text:"Elsa",
-                correct: false,
-            },
-            {
-                text:"Cinderella",
-                correct: true,
-            },
-            {
-                text:"Sleeping Beauty",
-                correct: false,
-            },
-            {
-                text:"Pocahontas",
-                correct: false,
-            }
-        ]
-    }
-]
+const questionsAndAnswers = [
+  {
+    id: 1,
+    question:
+      "In the UK, the abbreviation NHS stands for National what Service?",
+    answers: [
+      {
+        text: "Health",
+        correct: true,
+      },
+      {
+        text: "Honour",
+        correct: false,
+      },
+      {
+        text: "Household",
+        correct: false,
+      },
+      {
+        text: "Humanity",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 2,
+    question:
+      "Which of these brands was chiefly associated with the manufacture of household locks?",
+    answers: [
+      {
+        text: "Phillips",
+        correct: false,
+      },
+      {
+        text: "Flymo",
+        correct: false,
+      },
+      {
+        text: "Ronseal",
+        correct: false,
+      },
+      {
+        text: "Chubb",
+        correct: true,
+      },
+    ],
+  },
+  {
+    id: 3,
+    question:
+      "What name is given to the revolving belt machinery in an airport that delivers checked luggage from the plane to baggage reclaim?",
+    answers: [
+      {
+        text: "Carousel",
+        correct: true,
+      },
+      {
+        text: "Hangar",
+        correct: false,
+      },
+      {
+        text: "Concourse",
+        correct: false,
+      },
+      {
+        text: "Terminal",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 4,
+    question:
+      "Which Disney character famously leaves a glass slipper behind at a royal ball?",
+    answers: [
+      {
+        text: "Elsa",
+        correct: false,
+      },
+      {
+        text: "Cinderella",
+        correct: true,
+      },
+      {
+        text: "Sleeping Beauty",
+        correct: false,
+      },
+      {
+        text: "Pocahontas",
+        correct: false,
+      },
+    ],
+  },
 
-export default questionsAndAnswers
+  {
+    id: 5,
+    question:
+      "The hammer and sickle is one of the most recognisable symbols of which political ideology?",
+    answers: [
+      {
+        text: "Republicanism",
+        correct: false,
+      },
+      {
+        text: "Communism",
+        correct: true,
+      },
+      {
+        text: "Conservatism",
+        correct: false,
+      },
+      {
+        text: "Liberalism",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 6,
+    question: "Which company owns CHATGPT?",
+    answers: [
+      {
+        text: "Microsoft",
+        correct: true,
+      },
+      {
+        text: "Apple",
+        correct: false,
+      },
+      {
+        text: "Tesla",
+        correct: false,
+      },
+      {
+        text: "Airtel",
+        correct: false,
+      },
+    ],
+  },
+
+  {
+    id: 7,
+    question: "Who created this APP?",
+    answers: [
+      {
+        text: "Mazi Chichebe",
+        correct: true,
+      },
+      {
+        text: "Okonkwo",
+        correct: false,
+      },
+      {
+        text: "Okoro",
+        correct: false,
+      },
+      {
+        text: "Udengene",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 8,
+    question: "Igbo nation is? ",
+    answers: [
+      {
+        text: "A tribe",
+        correct: true,
+      },
+      {
+        text: "An ethnic group",
+        correct: false,
+      },
+      {
+        text: "A country",
+        correct: false,
+      },
+      {
+        text: "A state",
+        correct: false,
+      },
+    ],
+  },
+
+  {
+    id: 9,
+    question: "Who will be the next President of Nigeria? ",
+    answers: [
+      {
+        text: "Asiwaju Bola Ahmed",
+        correct: false,
+      },
+      {
+        text: "Peter Obi Gregory",
+        correct: false,
+      },
+      {
+        text: "Only God knows",
+        correct: true,
+      },
+      {
+        text: "Buhari Mohammadu",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 10,
+    question: "Do you think AI will take human job in the future? ",
+    answers: [
+      {
+        text: "Yes",
+        correct: false,
+      },
+      {
+        text: "Will only assist human reaching the goals and potentials",
+        correct: true,
+      },
+      {
+        text: "Maybe",
+        correct: false,
+      },
+      {
+        text: "Definitely",
+        correct: false,
+      },
+    ],
+  },
+];
+
+export default questionsAndAnswers;
